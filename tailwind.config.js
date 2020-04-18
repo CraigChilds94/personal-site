e49f7e64aa837a2d2f465b31997397e04ecc4078
Craig Childs <childscraig17@gmail.com>
2020-04-18 16:29:15 +0100

    Blog page styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+    fontFamily: {
+      serif: ['"Roboto Slab"', "sans-serif"],
+      sans: ['"Open Sans"', "sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
