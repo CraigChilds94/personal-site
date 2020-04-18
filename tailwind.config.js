c8af3a858ba9d39adb1777071a608a0eea78dd18
Craig Childs <childscraig17@gmail.com>
2020-04-18 17:48:43 +0100

    Styling code blocks, quotes etc and add a color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,29 @@
 module.exports = {
   theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: "2rem"
-    },
-    fontFamily: {
-      serif: ['"Roboto Slab"', "sans-serif"],
-      sans: ['"Open Sans"', "sans-serif"],
+    extend: {
+      colors: {
+        primary: "#66cccc",
+        primaryDarker: "#3eb9b9",
+        primaryDarkest: "#2f8d8d",
+        secondary: "#cccc66",
+        secondaryDarker: "#b9b93e",
+        secondaryDarkest: "#8d8d2f",
+        tertiary: "#6666cc",
+        tertiaryDarker: "#4949c2",
+        tertiaryDarkest: "#3939ab",
+        grey: "#a3a3aa",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+      },
+      fontFamily: {
+        serif: ['"Roboto Slab"', "sans-serif"],
+        sans: ['"Open Sans"', "sans-serif"],
+      },
+      height: {
+        104: "24rem",
+      }
     },
   },
   variants: {},
